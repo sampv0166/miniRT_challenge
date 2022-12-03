11c835979a4faeccce49eea81a453456f40fa130
abdu <sampv0166@gmail.com>
2022-12-03 19:56:32 +0400

    parse light fix

--- srcs/parser/p_light.c
+++ srcs/parser/p_light.c
@@ -1,24 +1,24 @@
 #include "../../includes/minirt.h"
 
-static int verify_digits(char **pnt_split, char **color_split ,char **info, t_data *scene_data)
-{
-    int i;
+// static int verify_digits(char **pnt_split, char **color_split ,char **info, t_data *scene_data)
+// {
+//     int i;
 
-    i = 0;
-    while (i < 3)
-    {
-        if (skip_dot_verify_digits(color_split[i]) || skip_dot_verify_digits(pnt_split[i]))
-        {
-            return (0);
-        }
-        i++;
-    }
-    if (skip_dot_verify_digits(info[2]))
-        return(0); 
-    if (scene_data->light_src.ratio < 0 || scene_data->light_src.ratio > 1)
-        return (0);     
-    return(1);
-}
+//     i = 0;
+//     while (i < 3)
+//     {
+//         if (skip_dot_verify_digits(color_split[i]) || skip_dot_verify_digits(pnt_split[i]))
+//         {
+//             return (0);
+//         }
+//         i++;
+//     }
+//     if (skip_dot_verify_digits(info[2]))
+//         return(0); 
+//     if (scene_data->light_src.ratio < 0 || scene_data->light_src.ratio > 1)
+//         return (0);     
+//     return(1);
+// }
 
 
     // !REDUCE TO 25 LINES  - SAMAD
@@ -43,22 +43,20 @@ int parse_light(char **info, t_data *scene_data, char **point_split, char **colo
         return (set_error_obj(2, "LIGHT POINT SHOULD HAVE 3 VALUES",scene_data));
     if (get_2darray_size(color_split) != 3)
         return (set_error_obj(2, "LIGHT COLOR SHOULD HAVE 3 VALUES",scene_data));
-    if (verify_digits(point_split, color_split, info, scene_data) &&  parse_color(info[3], &scene_data->light_src.color))
-    {
-        scene_data->light_src.pos.x = parse_double(point_split[0]);
-        scene_data->light_src.pos.y = parse_double(point_split[1]);
-        scene_data->light_src.pos.z = parse_double(point_split[2]);
+   if (!verify_digits(point_split))
+        return (set_error_obj(2, "ONLY DIGITS ALLOWED IN CAMERA POINT VALUES",scene_data));  
+    if (!verify_digits(color_split))
+        return (set_error_obj(2, "ONLY DIGITS ALLOWED IN CAMERA NORM VECTOR VALUES",scene_data));
 
-        scene_data->light_src.color.r = scene_data->light_src.color.r/ 255;
-	    scene_data->light_src.color.g  = scene_data->light_src.color.g/ 255;
-	    scene_data->light_src.color.b = scene_data->light_src.color.b/ 255;
-        scene_data->num_objs.num_light += 1;
+    scene_data->light_src.pos.x = parse_double(point_split[0]);
+    scene_data->light_src.pos.y = parse_double(point_split[1]);
+    scene_data->light_src.pos.z = parse_double(point_split[2]);
+
+    scene_data->light_src.color.r = scene_data->light_src.color.r/ 255;
+    scene_data->light_src.color.g  = scene_data->light_src.color.g/ 255;
+    scene_data->light_src.color.b = scene_data->light_src.color.b/ 255;
+    scene_data->num_objs.num_light += 1;
         // normmalize the light color here
-    }
-    else
-    {
-        return (set_error_obj(2, "ONLY DIGITS ARE ALLOWED IN VALUES FOR LIGHT",scene_data));
-    }
     scene_data->num_objs.num_light += 1;
     return (1);
 }
