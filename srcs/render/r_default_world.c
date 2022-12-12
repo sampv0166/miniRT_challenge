a80bac37963b593797c0d878cf6cccaafe632caf
Imran <imzs@hotmail.com>
2022-12-12 13:36:54 +0400

    fixing lighting functions

--- srcs/render/r_default_world.c
+++ srcs/render/r_default_world.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   r_default_world.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apila-va <apila-va@student.42.fr>          +#+  +:+       +#+        */
+/*   By: imustafa <imustafa@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 18:28:00 by imustafa          #+#    #+#             */
-/*   Updated: 2022/12/11 15:28:10 by apila-va         ###   ########.fr       */
+/*   Updated: 2022/12/12 13:36:23 by imustafa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	default_world(t_data *scene_data)
 	scene_data->wrld.l = point_light(scene_data->light_src.pos,
 			scene_data->light_src.color, scene_data->light_src.ratio);
 	scene_data->wrld.l.ratio =  scene_data->light_src.ratio;
+	scene_data->wrld.l.ambient_ratio =  scene_data->amb_ratio;
 	// scene_data->wrld.l.pos = scene_data->light_src.pos;
 	// scene_data->wrld.l.color.r = scene_data->wrld.l.color.r * scene_data->wrld.l.ratio;		
 	// scene_data->wrld.l.color.g = scene_data->wrld.l.color.g * scene_data->wrld.l.ratio;
